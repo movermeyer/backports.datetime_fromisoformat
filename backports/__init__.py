5fe1e8052f45f0227dcba74a40230b7c77981dcf
Michael Overmeyer <m.overmeyer@yahoo.ca>
2018-06-29 16:14:38 -0400

    Added comment explaining the namespace package quirkiness.

--- backports/__init__.py
+++ backports/__init__.py
@@ -1,3 +1,5 @@
-https://packaging.python.org/guides/packaging-namespace-packages/#pkgutil-style-namespace-packages
+# This is a namespace package so that `backports.datetime_fromisoformat` can integrate 
+# nicely with the other `backports` modules.
+# See https://packaging.python.org/guides/packaging-namespace-packages/#pkgutil-style-namespace-packages
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
