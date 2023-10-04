b1db9fc186563e5829da31228be82cfbee1a41fa
Michael Overmeyer <michael@overmeyer.io>
2023-10-04 11:45:40 -0400

    Use pyproject.toml, tox, and pytest

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 import os
 
 from setuptools import setup, Extension
-# workaround for open() with encoding='' python2/3 compatibility
-from io import open
-
-with open('README.rst', encoding='utf-8') as file:
-    long_description = file.read()
 
 # We want to force all warnings to be considered errors. That way we get to catch potential issues during
 # development and at PR review time.
@@ -30,18 +25,7 @@ if os.environ.get("STRICT_WARNINGS", "0") == "1":
         os.environ["_CL_"] = ""
     os.environ["_CL_"] += " /WX"
 
-VERSION = "2.0.0"
-
 setup(
-    name="backports-datetime-fromisoformat",
-    version=VERSION,
-    description="Backport of Python 3.11's datetime.fromisoformat",
-    long_description=long_description,
-    license="MIT",
-    author="Michael Overmeyer",
-    author_email="backports@movermeyer.com",
-    url="https://github.com/movermeyer/backports.datetime_fromisoformat",
-
     packages=["backports", "backports.datetime_fromisoformat"],
 
     ext_modules=[Extension("backports._datetime_fromisoformat", [
@@ -49,24 +33,4 @@ setup(
         os.path.join("backports", "datetime_fromisoformat", "_datetimemodule.c"),
         os.path.join("backports", "datetime_fromisoformat", "timezone.c")
     ])],
-
-    test_suite='tests',
-    tests_require=[
-        'pytz'
-    ],
-    classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
 )
